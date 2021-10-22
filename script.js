05b46423230113eeeccb49b0648da07b20e69986
Brad <brt.jst@gmail.com>
2021-10-22 13:33:57 -0700

    improve css, html

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-function square(int) {
- const result = int * int;
-  
-  console.log("answer", result);
+"use strict";
 
-  return result;
-}
+// Codewars Find min and max in an array
+
+// let array = [-52, 56, 30, 29, -54, 0, -110];
+
+// const min = (list) => Math.min(...list);
+
+// const max = (list) => Math.max(...list);
+
+// console.log(min([-52, 56, 30, 29, -54, 0, -110]));
+// console.log(max([-52, 56, 30, 29, -54, 0, -110]));
+
+// Codewars Array plus Array
+
+// function arrayPlusArray(arr1, arr2) {
+//     const reducer = (previousValue, currentValue) => previousValue + currentValue;
+
+//     return arr1.concat(arr2).reduce(reducer);
+//   }
+//   console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]))
+
+// CodeWars String repeat
+
+// const repeatStr = (multiplier, str) => `${str.repeat(multiplier)}`
+
+// console.log(repeatStr(4, " blow me"))
