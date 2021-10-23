f4b631d6eb06fe364ae2c6555f3b9290a6de1814
Brad <brt.jst@gmail.com>
2021-10-23 16:19:36 -0700

    katas 10-23

--- script.js
+++ script.js
@@ -81,7 +81,84 @@
 // const isSquare = function(n){
 //     return n >= 0 && Math.sqrt(n) %1 === 0;
 //     //  %1 (modulus 1 returns a whole number), 0 is a square root
-     
+
 //   }
 
 //   console.log(isSquare(10));
+
+// Reversed Words
+
+// const reverseWords = (str) => str.split('').reverse().join('');
+
+// console.log(reverseWords("P*KRlCadCe1){4x!T;GR5:kgJ1iW\,(0a+Qhmc&pnW/kx!#3C_%?"))
+
+// Transportation on vacation
+
+// function rentalCarCost(days) {
+//   if (days >= 7) {
+//     return days * 40 - 50;
+//   } else if (days >= 3 && days < 7) {
+//     return days * 40 - 20;
+//   } else {
+//     return days * 40;
+//   }
+// }
+// console.log(rentalCarCost(7));
+// console.log(rentalCarCost(1));
+// console.log(rentalCarCost(10));
+// console.log(rentalCarCost(5));
+
+// You only need one - Beginner
+
+// function check(array, value) {
+//     if (array.includes(value)) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+
+// }
+
+// function check(array, value) {
+//   return array.includes(value) ? true : false;
+// }
+
+// console.log(check([-52, 56, 30, 29, -54, 0, -110], 1));
+
+// Century From Year
+
+// const century = function (year) {
+//   return Math.floor(year / 100) + (year % 100 ? 1 : 0);
+// };
+
+// console.log(century(1901));
+
+// Basic Mathematical Operations
+
+// const basicOp = (operator, value1, value2) =>
+// {switch (operator) {
+//   case '+': return value1 + value2;
+//   case '-': return value1 - value2;
+//   case '*': return value1 * value2;
+//   case '/': return value1 / value2;
+// }
+// }
+
+// console.log(basicOp('-', 3, 6));
+
+//  Double Char
+
+// function doubleChar(str) {
+//   let doubleString = "";
+
+//   for (let i = 0; i < str.length; i++) {
+//     doubleString += str[i] + str[i];
+//   }
+//   return doubleString;
+// }
+
+// console.log(doubleChar("Let's go Brandon!"));
+
+
+    
+ 
