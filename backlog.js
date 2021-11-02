ff7c9a5ee9877d7a3c1a8343d63ccb8b9fb4d1a9
Brad <brt.jst@gmail.com>
2021-11-02 05:32:12 -0700

    practice kata

--- backlog.js
+++ backlog.js
@@ -347,3 +347,9 @@
 // const opposite = (number) => number * -1;
 
 // console.log(opposite(-14))
+
+// Even or Odd
+
+// const even_or_odd = (number) => number %2 === 0 ? "Even" : "Odd";
+
+// console.log(even_or_odd(16))
--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 "use strict";
 
+// // Even or Odd
+
+// const even_or_odd = (number) => number %2 === 0 ? "Even" : "Odd";
+
+// console.log(even_or_odd(16))
