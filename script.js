8cb90649922442007c7bf613ca2d24a511499584
Brad <brt.jst@gmail.com>
2021-11-07 18:20:17 -0700

    sandbox code-jam solution

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 "use strict";
-
-
   
+// gpa function that returns integer.1st decimal -  CODEJAM
 
+// const rando = () => (Math.random() * 3 + 1);
+// console.log(rando.slice(-9));
+// let randomGPA = () => Array.from(rando()).slice(-9)
+ 
+
+// console.log(randomGPA());
+// console.log(randomGPA());
+// console.log(randomGPA());
+// console.log(randomGPA());
+// console.log(randomGPA());
+// console.log(randomGPA());
+// console.log(randomGPA());
+// console.log(randomGPA());
+// console.log(randomGPA());
+// console.log(randomGPA());
+    
+
+// Improve firstName/lastName function CODEJAM
+
+// const firstNames = ["Brad 1", "Zintis 2", "Joe 3", "Phoebe 4", "Giuseppe 5"];
+
+// const fName = () =>  Math.trunc(Math.random() * firstNames.length);
+
+// console.log(firstNames[fName()])
+// console.log(firstNames[fName()])
+// console.log(firstNames[fName()])
+// console.log(firstNames[fName()])
+// console.log(firstNames[fName()])
+
+   
