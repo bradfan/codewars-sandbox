2b59f6ec1d917e1d4598eaabe96241bdbe07801f
Brad <brt.jst@gmail.com>
2021-11-09 17:45:40 -0700

    update backlog

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 "use strict";
 
-
+// Generate number 3 - 18 using 1-6 * 3
+const bellCurve = () => (Math.trunc(Math.random() * 6 + 1) * 3)
+console.log(bellCurve());
+console.log(bellCurve());
+console.log(bellCurve());
+console.log(bellCurve());
+console.log(bellCurve());
+console.log(bellCurve());
 
 
   
