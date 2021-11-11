159e911ba41fcf23be85cc5d05067ebf14dbe0ce
Brad <brt.jst@gmail.com>
2021-11-10 17:03:10 -0700

    sandbox

--- backlog.js
+++ backlog.js
@@ -467,3 +467,12 @@
 
 // console.log(gooseFilter(["Mallard","Hook Bill","African","Crested","Pilgrim","Toulouse","Blue Swedish",])
 // );
+
+// Generate number 3 - 18 using 1-6 * 3
+// const bellCurve = () => (Math.trunc(Math.random() * 6 + 1) * 3)
+// console.log(bellCurve());
+// console.log(bellCurve());
+// console.log(bellCurve());
+// console.log(bellCurve());
+// console.log(bellCurve());
+// console.log(bellCurve());
