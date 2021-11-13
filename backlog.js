b697c57ba63e4ceeb31ef357cb3b950f44906b58
Brad <brt.jst@gmail.com>
2021-11-12 17:51:10 -0700

    code snippets and apractice kata

--- backlog.js
+++ backlog.js
@@ -484,3 +484,153 @@
 
 
 // console.log(hoopCount(15))
+
+// CODE SNIPPET RE: DYNAMICALLY CREATED RADIO BUTTONS
+
+// let fighterBtn = document.createElement("input");
+// let fighterBtnLbl = document.createElement("label");
+// fighterBtnLbl.for = "fighterBtn";
+// fighterBtnLbl.innerHTML = "Fighter  ";
+// classForm.append(fighterBtnLbl);
+// fighterBtn.type = "radio";
+// fighterBtn.name = "profession";
+// fighterBtn.onclick = "formFunction(this.value)";
+// fighterBtn.value = "Fighter";
+
+// classForm.append(fighterBtn);
+
+// let palladinBtn = document.createElement("input");
+// let palladinBtnLbl = document.createElement("label");
+// palladinBtnLbl.for = "palladinBtn";
+// palladinBtnLbl.innerHTML = "Palladin  ";
+// classForm.append(palladinBtnLbl);
+// palladinBtn.type = "radio";
+// palladinBtn.name = "profession";
+// palladinBtn.onclick = "formFunction(this.value)";
+// palladinBtn.value = "Palladin";
+
+// classForm.append(palladinBtn);
+
+// let rangerBtn = document.createElement("input");
+// let rangerBtnLbl = document.createElement("label");
+// rangerBtnLbl.for = "rangerBtn";
+// rangerBtnLbl.innerHTML = "Ranger  ";
+// classForm.append(rangerBtnLbl);
+// rangerBtn.type = "radio";
+// rangerBtn.name = "profession";
+// rangerBtn.onclick = "formFunction(this.value)";
+// rangerBtn.value = "Ranger";
+
+// classForm.append(rangerBtn);
+
+// let clericBtn = document.createElement("input");
+// let clericBtnLbl = document.createElement("label");
+// clericBtnLbl.for = "clericBtn";
+// clericBtnLbl.innerHTML = "Cleric  ";
+// classForm.append(clericBtnLbl);
+// clericBtn.type = "radio";
+// clericBtn.name = "profession";
+// clericBtn.onclick = "formFunction(this.value)";
+// clericBtn.value = "Cleric";
+
+// classForm.append(clericBtn);
+
+// let druidBtn = document.createElement("input");
+// let druidBtnLbl = document.createElement("label");
+// druidBtnLbl.for = "druidBtn";
+// druidBtnLbl.innerHTML = "Druid  ";
+// classForm.append(druidBtnLbl);
+// druidBtn.type = "radio";
+// druidBtn.name = "profession";
+// druidBtn.onclick = "formFunction(this.value)";
+// druidBtn.value = "druid";
+
+// classForm.append(druidBtn);
+
+// let magicUserBtn = document.createElement("input");
+// let magicUserBtnLbl = document.createElement("label");
+// magicUserBtnLbl.for = "magicUserBtn";
+// magicUserBtnLbl.innerHTML = "Magic-User  ";
+// classForm.append(magicUserBtnLbl);
+// magicUserBtn.type = "radio";
+// magicUserBtn.name = "profession";
+// magicUserBtn.onclick = "formFunction(this.value)";
+// magicUserBtn.value = "MagicUser";
+
+// classForm.append(magicUserBtn);
+
+// let illusionistBtn = document.createElement("input");
+// let illusionistBtnLbl = document.createElement("label");
+// illusionistBtnLbl.for = "illusionistBtn";
+// illusionistBtnLbl.innerHTML = "Illusionist  ";
+// classForm.append(illusionistBtnLbl);
+// illusionistBtn.type = "radio";
+// illusionistBtn.name = "profession";
+// illusionistBtn.onclick = "formFunction(this.value)";
+// illusionistBtn.value = "Illusionist";
+
+// classForm.append(illusionistBtn);
+
+// let thiefBtn = document.createElement("input");
+// let thiefBtnLbl = document.createElement("label");
+// thiefBtnLbl.for = "thiefBtn";
+// thiefBtnLbl.innerHTML = "Thief  ";
+// classForm.append(thiefBtnLbl);
+// thiefBtn.type = "radio";
+// thiefBtn.name = "profession";
+// thiefBtn.onclick = "formFunction(this.value)";
+// thiefBtn.value = "Thief";
+
+// classForm.append(thiefBtn);
+
+// let assasinBtn = document.createElement("input");
+// let assasinBtnLbl = document.createElement("label");
+// assasinBtnLbl.for = "assasinBtn";
+// assasinBtnLbl.innerHTML = "Assasin  ";
+// classForm.append(assasinBtnLbl);
+// assasinBtn.type = "radio";
+// assasinBtn.name = "profession";
+// assasinBtn.onclick = formFunction(this.value);
+// assasinBtn.value = "Assasin";
+
+// classForm.append(assasinBtn);
+
+// let submitClass = document.createElement("input");
+// submitClass.type = "text";
+// submitClass.id = "result";
+// submitClass.placeholder = "Your selection.";
+// classForm.append(submitClass);
+// let submitClass2 = document.createElement("input");
+// submitClass2.type = "submit";
+// submitClass2.value = "Submit class";
+
+// classForm.append(submitClass2);
+
+// this function (also saved in sandbox) returns cannot read properties of null. It works in standard html/js but not dynamically. It may be out of place.
+     // function formFunction(profession) {
+     //   console.log("Profession", profession);
+     //   document.getElementById("result").value = profession;
+     // }
+
+
+// I love you, a little , a lot, passionately ... not at all CODEWARS
+
+// const howMuchILoveYou = (nb_petals) => {
+//      switch (nb_petals % 6) {
+//        case 0:
+//          return "not at all";
+//        case 1:
+//          return "I love you";
+//        case 2:
+//          return "a little";
+//        case 3:
+//          return "a lot";
+//        case 4:
+//          return "passionately";
+//        case 5:
+//          return "madly";
+//      }
+//    };
+   
+   
+//    console.log(howMuchILoveYou(7));
