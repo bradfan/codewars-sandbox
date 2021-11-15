c6a8dc35a9dcefea3e485f0f3661bf138b3c96f6
Brad <brt.jst@gmail.com>
2021-11-15 14:00:47 -0700

    sample code for project

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-"use strict";
+ "use strict";
 
 
+document.getElementById('generate').onclick = function() {
+ 
+    let values = ["dog", "cat", "parrot", "rabbit"];
+ 
+    const select = document.createElement("select");
+    select.name = "pets";
+    select.id = "pets"
+ 
+    for (const val of values)
+    {
+        var option = document.createElement("option");
+        option.value = val;
+        option.text = val.charAt(0).toUpperCase() + val.slice(1);
+        select.appendChild(option);
+    }
+ 
+    const label = document.createElement("label");
+    label.innerHTML = "Choose your pets: "
+    label.htmlFor = "pets";
+ 
+    document.getElementById("container").appendChild(label).appendChild(select);
+}
