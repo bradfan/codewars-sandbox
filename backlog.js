6f6d5be8e57f1d44b376fde6ed6a11d1172d477e
Brad <brt.jst@gmail.com>
2021-11-23 13:09:31 -0700

    practice kata

--- backlog.js
+++ backlog.js
@@ -642,3 +642,23 @@
   
 
 //   console.log(cockroachSpeed(100))
+
+// Drink about
+
+// const peopleWithAgeDrink = function (old) {
+//      if(old < 14) {
+//          return "drink toddy";
+//      } else if(old < 18) {
+//         return "drink coke";
+//      } else if(old < 21) {
+//          return "drink beer";
+//      } else return 'drink whisky';
+//    };
+ 
+//    console.log(peopleWithAgeDrink());
+ 
+ //   Abbreviated solution:
+ //   const peopleWithAgeDrink = (age) =>
+ //   age < 14 ? "drink toddy" :
+ //   age < 18 ? "drink coke" :
+ //   age < 21 ? "drink beer" : "drink whisky"
