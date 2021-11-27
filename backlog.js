7a13a9465316521e1e1f61478e5e54d11765b38b
Brad <brt.jst@gmail.com>
2021-11-26 17:57:57 -0700

    practice

--- backlog.js
+++ backlog.js
@@ -662,3 +662,40 @@
  //   age < 14 ? "drink toddy" :
  //   age < 18 ? "drink coke" :
  //   age < 21 ? "drink beer" : "drink whisky"
+
+ // Switch it up
+
+// const switchItUp = function (number) {
+//   switch (number) {
+//     case 0:
+//       return "Zero";
+//     case 1:
+//       return "One";
+
+//     case 2:
+//       return "Two";
+
+//     case 3:
+//       return "Three";
+
+//     case 4:
+//       return "Four";
+
+//     case 5:
+//       return "Five";
+
+//     case 6:
+//       return "Six";
+
+//     case 7:
+//       return "Seven";
+
+//     case 8:
+//       return "Eight";
+
+//     case 9:
+//       return "Nine";
+//   }
+// };
+
+// console.log(switchItUp(5));
--- script.js
+++ script.js
@@ -1,14 +1,13 @@
- "use strict";
-
+"use strict";
 
 // document.getElementById('generate').onclick = function() {
- 
+
 //     let values = ["dog", "cat", "parrot", "rabbit"];
- 
+
 //     const select = document.createElement("select");
 //     select.name = "pets";
 //     select.id = "pets"
- 
+
 //     for (const val of values)
 //     {
 //         var option = document.createElement("option");
@@ -16,11 +15,25 @@
 //         option.text = val.charAt(0).toUpperCase() + val.slice(1);
 //         select.appendChild(option);
 //     }
- 
+
 //     const label = document.createElement("label");
 //     label.innerHTML = "Choose your pets: "
 //     label.htmlFor = "pets";
- 
+
 //     document.getElementById("container").appendChild(label).appendChild(select);
 // }
+
+// HACKER RANK TEST QUIZ fizzbuzz
+
+// function fizzBuzz(n) {
+//   for (let i = 1; i < n + 1; i++) {
+//     if (i % 15 === 0) console.log(i, "fizzbuzz");
+//     else if (i % 3 === 0) console.log(i, "fizz");
+//     else if (i % 5 === 0) console.log(i, "buzz");
+//     else console.log(i);
+//   }
+// }
+
+// console.log(fizzBuzz(15));
+
 
